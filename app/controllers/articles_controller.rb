d210f87a0569b9ec5a9931268f80a82470158278
SanthoshRam3 <santhoshram03111998@gmail.com>
2022-07-25 16:14:17 +0530

    Validations and updates in the articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,26 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new   
   end
-
+  def edit
+    
+    @article = Article.find(params[:id])
+  end
   def create
-    @article = Article.new (params.require(:article).permit(:title,:description))
+    @article = Article.new(params.require(:article).permit(:title,:description))
     if @article.save
       flash[:notice] = "Article was succesfully created."
     redirect_to @article
     else
       render 'new'
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notice]="Article Was successfully updated"
+      redirect_to @article
+    else
+      render'edit'
     end
   end
 end
