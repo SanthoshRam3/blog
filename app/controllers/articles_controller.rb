6886940d8e020355534885eed38f5826b74a3f4a
SanthoshRam3 <santhoshram03111998@gmail.com>
2022-07-29 10:55:16 +0530

    CRUD operation to destroy an action in an articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def show
-    
     @article = Article.find(params[:id])
   end
   def index
@@ -30,5 +29,10 @@ class ArticlesController < ApplicationController
     else
       render'edit'
     end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
